3c8e59fedf52b4f60a073b1179efcb44bd56c9d3
Holly <hollykaboaz@gmail.com>
2022-11-05 17:53:24 -0400

    Treasure Data and PowerUp Item Class Created
    in progress of building treasure chest feature

---
+++ src/PowerUp.java
@@ -0,0 +1,25 @@
+public class PowerUp extends Item {
+    int points;
+    String type;
+
+    public PowerUp(String itemName, String itemDescription, String type, int points) {
+        super(itemName, itemDescription);
+        this.points = points;
+        this.type = type.toLowerCase();
+    }
+
+    public void activate(Player player) {
+        switch (type){
+            case "strength":
+                player.setStrength(player.getStrength() + points);
+                break;
+            case "speed":
+                player.setSpeed(player.getSpeed() + points);
+                break;
+            case "health":
+                player.setHealth(player.getHealth() + points);
+                break;
+        }
+    }
+
+}
